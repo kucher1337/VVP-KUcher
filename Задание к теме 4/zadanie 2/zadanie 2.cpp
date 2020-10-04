4fa3533e4de23602bd71d4414668acc6a3adb8fd
re re <kuchernik2002@gmail.com>
2020-10-04 17:06:24 +0300

    Zadanie 2

---
+++ Задание к теме 4/zadanie 2/zadanie 2.cpp
@@ -0,0 +1,13 @@
+﻿#include <stdio.h> //Подключение библиотеки ввода/вывода
+
+#include <stdlib.h> //Подключение библиотеки
+
+
+int main()
+{
+	double d = 3, π = 3.14, L;
+	L = d * π;
+	printf("%f\n", L);
+	system("pause");
+	return 0;
+}
