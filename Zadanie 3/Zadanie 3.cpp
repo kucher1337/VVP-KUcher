f7d322c8f148fd9ae57713273c80c80a21b3c519
re re <kuchernik2002@gmail.com>
2020-10-04 17:14:44 +0300

    Zadanie 3

---
+++ Zadanie 3/Zadanie 3.cpp
@@ -0,0 +1,13 @@
+﻿#include <stdio.h> //Подключение библиотеки ввода/вывода
+
+#include <stdlib.h> //Подключение библиотеки
+
+
+int main()
+{
+	double a = 3, b = 5, S;
+	S = (a + b) / 2;
+	printf("%f\n", S);
+	system("pause");
+	return 0;
+}
