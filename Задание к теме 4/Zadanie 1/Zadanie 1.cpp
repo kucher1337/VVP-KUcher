f83d2fbd78a242afac6a67887705f6c6293f691f
re re <kuchernik2002@gmail.com>
2020-10-04 16:55:39 +0300

    zadanie 1

---
+++ Задание к теме 4/Zadanie 1/Zadanie 1.cpp
@@ -0,0 +1,16 @@
+﻿#include <stdio.h> 
+
+#include <stdlib.h> 
+
+
+int main()
+{
+	double a = 3, b = 5, S, P;
+
+	S = a * b;
+	printf("%f\n", S);
+	P = 2 * (a + b);
+	printf("%f\n", P);
+	system("pause");
+	return 0;
+}
