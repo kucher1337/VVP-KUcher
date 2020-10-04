e6000d99b28ca3a0db1f8a98e0fd10e6cac734b7
re re <kuchernik2002@gmail.com>
2020-10-04 17:41:27 +0300

    zadanie 4

---
+++ Задание к теме 4/zadanie 4/zadanie 4.cpp
@@ -0,0 +1,18 @@
+﻿#include <stdio.h> //Подключение библиотеки ввода/вывода
+#include <math.h>
+#include <stdlib.h> //Подключение библиотеки
+
+int main()
+{
+	double a = 3, b = 1, S, A, E, K;
+	S = pow(a, 2)  + pow(b, 2);
+	printf("%lf\n", S);
+	A = pow(a, 2) - pow(b, 2) *4;
+	printf("%lf\n", A);
+	E = pow(a, 2) * pow(b, 2) / 2;
+	printf("%lf\n", E) + 1;
+	K = pow(a, 2) / pow(b, 2);
+	printf("%lf\n", K);
+	system("pause");
+	return 0;
+}
