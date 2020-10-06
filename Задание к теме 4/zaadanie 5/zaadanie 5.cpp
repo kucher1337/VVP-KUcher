71eb750b8bc7d4a4508af09ed099f00aa32ce9ac
re re <kuchernik2002@gmail.com>
2020-10-06 11:01:10 +0300

    Zadanie5

---
+++ Задание к теме 4/zaadanie 5/zaadanie 5.cpp
@@ -0,0 +1,18 @@
+﻿#include <stdio.h> //Подключение библиотеки ввода/вывода
+#include <math.h>
+#include <stdlib.h> //Подключение библиотеки
+
+int main()
+{
+	double a = 3, b = 5, S, A, E, K;
+	S = fabs(a) - fabs(b) - 4;
+	printf_s("%lf\n", S);
+	A = (fabs(a) + fabs(b)) * 3;
+	printf_s("%lf\n", A);
+	E = (fabs(a) * fabs(b))*3;
+	printf_s("%lf\n", E);
+	K = (fabs(a) / fabs(b)) * 3;
+	printf_s("%lf\n", K);
+	system("pause");
+	return 0;
+	}
