08b885d015126d435db219ca94a8de23c8d089b7
re re <kuchernik2002@gmail.com>
2020-10-17 19:38:22 +0300

    Задание 1

---
+++ Задание к теме 5/ConsoleApplication4/ConsoleApplication4.cpp
@@ -0,0 +1,13 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+int main()
+{
+	double x1 = 3, y1 = 5, x2 = 2, y2 = 3, s;
+	s = sqrt(pow(x1 - x2, 2) + pow(y1 - y2, 2));
+		printf("%f\n", s);
+		system("pause");
+		return 0;
+}
