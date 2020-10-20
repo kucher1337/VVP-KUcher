783fcf4a0da1d0b1c5cd1a35db78d9e1a22128dd
re re <kuchernik2002@gmail.com>
2020-10-20 17:17:20 +0300

    Задание 3

---
+++ Задание к теме 5/Задание 5/Задание 3.cpp
@@ -0,0 +1,17 @@
+﻿#include <stdlib.h>
+#include <math.h>
+#include <stdio.h>
+int main()
+{
+	double A = 42, B = 121, C = 61, AC, BC, P;
+	AC = fabs(C - A);
+	printf("%f\n", AC);
+	BC = fabs(C - B);
+	printf("%f\n", BC);
+	P = AC * BC;
+	printf("%f\n", P);
+	system("pause");
+	return 0;
+}
+
+	
