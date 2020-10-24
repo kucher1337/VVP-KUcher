5ebb602f447d36b25ee56657d1ac7704f1aaae04
re re <kuchernik2002@gmail.com>
2020-10-24 15:34:21 +0300

    Задание 5

--- Задание к теме 5/Задание 5(2)/Задание 5(2).cpp
+++ Задание к теме 5/Задание 5(2)/Задание 5(2).cpp
@@ -3,13 +3,14 @@
 #include <stdio.h>
 int main()
 {
-	double x1 = 4, y1 = 10, x2 = 7, y2 = 3, x3 = 5, y3 = 11, a, b, c, S, P;
+	double x1 = 4, y1 = 10, x2 = 7, y2 = 3, x3 = 5, y3 = 11, a, b, c, p, S, P;
 	a = sqrt((pow)(x1 - x2, 2) + (pow(y1 + y2, 2)));
 	b= sqrt((pow)(x2 - x3, 2) + (pow(y2 + y3, 2)));
 	c= sqrt((pow)(x1 - x3, 2) + (pow(y1 + y3, 2)));
 	P = a + b + c;
+	p = P / 2;
 	printf("%lf\n", P);
-	S = sqrt(P * (P - a) * (P - b) * (P - c));
+	S = sqrt(p * (p - a) * (p - b) * (p - c));
 	printf("%lf\n", S);
 	system("pause");
 	return 0;
