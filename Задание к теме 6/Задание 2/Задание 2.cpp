ecaa719304db0d8039cb4a7daeae92a05cd41df6
re re <kuchernik2002@gmail.com>
2020-11-01 21:07:44 +0300

    Задание 2

---
+++ Задание к теме 6/Задание 2/Задание 2.cpp
@@ -0,0 +1,16 @@
+﻿#include <stdlib.h>
+#include <stdio.h>
+int main()
+{
+	double A = 15, B = 14, C=13, n, k;
+	n = B;
+	B = A;
+	k = C;
+	C = n;
+	A = k;
+	printf("%lf\n", A);
+	printf("%lf\n", B);
+	printf("%lf\n", C);
+	system("pause");
+	return 0;
+}
