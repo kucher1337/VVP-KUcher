122a79a6281183297eb0369877bd0a01dec780a1
re re <kuchernik2002@gmail.com>
2020-11-01 21:19:47 +0300

    Задание 3

---
+++ Задание к теме 6/Задание 3/Задание 3.cpp
@@ -0,0 +1,16 @@
+﻿#include <stdlib.h>
+#include <stdio.h>
+int main()
+{
+	double A = 15, B = 14, C = 13, n, k;
+	n = C;
+	C = A;
+	k = B;
+	B = n;
+	A = k;
+	printf("%lf\n", A);
+	printf("%lf\n", B);
+	printf("%lf\n", C);
+	system("pause");
+	return 0;
+}
