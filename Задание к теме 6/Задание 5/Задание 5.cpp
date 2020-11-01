e889c5ad71d7f66f59a81b430e15970b7e0a4af6
re re <kuchernik2002@gmail.com>
2020-11-01 21:29:02 +0300

    Задание 5

---
+++ Задание к теме 6/Задание 5/Задание 5.cpp
@@ -0,0 +1,11 @@
+﻿#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+	double x = 3, y;
+	y = 4 * pow(x - 3, 6) - 7 * pow(x - 3, 3) + 2;
+	printf("%lf\n", y);
+	system("pause");
+	return 0;
+}
