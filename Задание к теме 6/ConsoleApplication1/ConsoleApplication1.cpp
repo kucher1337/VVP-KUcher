3c2bc131c50fae37c9297de2cb0c432e35cc612a
re re <kuchernik2002@gmail.com>
2020-11-01 21:02:48 +0300

    Задание 1

---
+++ Задание к теме 6/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,13 @@
+﻿#include <stdlib.h>
+#include <stdio.h>
+int main()
+{
+	double A = 15, B = 14, c;
+	c = A;
+	A = B;
+	B = c;
+	printf("%lf\n", A);
+	printf("%lf\n", B);
+	system("pause");
+	return 0;
+}
