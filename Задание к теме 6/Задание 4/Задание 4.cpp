b97d236068550155b26462806542431b2bdcb482
re re <kuchernik2002@gmail.com>
2020-11-01 21:25:18 +0300

    Задание 4

---
+++ Задание к теме 6/Задание 4/Задание 4.cpp
@@ -0,0 +1,12 @@
+﻿#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+	double x = 2, y;
+	y = 3 * pow(x, 6) - 6 * pow(x, 2) - 7;
+	printf("%lf\n", y);
+	system("pause");
+	return 0;
+
+}
