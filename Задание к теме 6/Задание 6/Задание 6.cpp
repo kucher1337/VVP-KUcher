83ba047979e4c17afe0fa047b284d4c4dd27e502
re re <kuchernik2002@gmail.com>
2020-11-03 01:21:30 +0300

    Задание 6

---
+++ Задание к теме 6/Задание 6/Задание 6.cpp
@@ -0,0 +1,12 @@
+﻿#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+	double A = 3, b;
+	b = A * A * A;
+	A = b * b * pow(A,2);
+	printf("%lf\n", A);
+	system("pause");
+	return 0;
+}
---
+++ Задание к теме 6/Задание 7/Задание 7.cpp
@@ -0,0 +1,14 @@
+﻿#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+	double A = 2, b, c;
+	b = A * A * A;
+	c = b * b;
+	c = c * c;
+	A = b * c;
+	printf("%lf\n", A);
+	system("pause");
+	return 0;
+}
