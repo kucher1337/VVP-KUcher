22385bf0c821bab8d7fd99e4a76ff4a2387c7f44
re re <kuchernik2002@gmail.com>
2020-11-05 17:33:19 +0300

    Задание 4

--- Задание к теме 7/Задание 4/Задание 4.cpp
+++ Задание к теме 7/Задание 4/Задание 4.cpp
@@ -3,17 +3,17 @@
 #include <locale.h>
 int main()
 {
-float v, e, s, t, S;
+float v1, v2, s, t, S;
 	setlocale(LC_ALL, "Rus");
-	printf_s("Скорость первого авто v:");
-	scanf_s("%f", &v);
-	printf_s("Скорость второго авто e:");
-	scanf_s("%f", &e);
+	printf_s("Скорость первого авто v1:");
+	scanf_s("%f", &v1);
+	printf_s("Скорость второго авто v2:");
+	scanf_s("%f", &v2);
 	printf_s("Расстояние между авто s:");
 	scanf_s("%f", &s);
 	printf_s("Время t:");
 	scanf_s("%f", &t);
-	S = s + v * t + e * t;
+	S = s + v1 * t + v2 * t;
 	printf_s("%lf\n", S);
 	system("pause");
 	return 0;
