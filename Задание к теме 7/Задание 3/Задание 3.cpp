3fccf76d0b70b7aa2daddc902fb3635b3a5671f4
re re <kuchernik2002@gmail.com>
2020-11-05 17:16:17 +0300

    Задание 3

---
+++ Задание к теме 7/Задание 3/Задание 3.cpp
@@ -0,0 +1,19 @@
+﻿#include <stdlib.h>
+#include <stdio.h>
+#include <locale.h>
+int main()
+{
+	float Y, A, X, l, R;
+	setlocale(LC_ALL, "Rus");
+	printf_s("A-кол-во конфет в кг: ");
+	scanf_s("%f", &A);
+	printf_s("X-цена в руб за A кг:");
+	scanf_s("%f", &X);
+	l = X / A;
+	printf_s("Y кг: ");
+	scanf_s("%f", &Y);
+	R = Y * l;
+	printf_s("%lf\n", R);
+	system("pause");
+	return 0;
+}
