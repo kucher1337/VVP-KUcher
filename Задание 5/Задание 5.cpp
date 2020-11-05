73df6eb138f6730e2215e58c67cb2bb467fb1117
re re <kuchernik2002@gmail.com>
2020-11-05 17:44:10 +0300

    Задание 5

---
+++ Задание 5/Задание 5.cpp
@@ -0,0 +1,14 @@
+﻿#include <stdlib.h>
+#include <stdio.h>
+#include <locale.h>
+int main()
+{
+	float A = 40, B = 100, x;
+	
+	if (A != 0) {
+		x = -B / A;
+		printf("%f\n", x);
+		}
+	return 0;
+	
+}
