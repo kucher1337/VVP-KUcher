76efb40204bb0cac802affd5ae30b10daae7101b
re re <kuchernik2002@gmail.com>
2020-11-05 14:47:48 +0300

    Задание 1

---
+++ Задание к теме 7/Задание 1/Задание 1.cpp
@@ -0,0 +1,14 @@
+﻿#include <stdlib.h>
+#include <stdio.h>
+#include <locale.h>
+int main()
+{
+	double a;
+	setlocale(LC_ALL, "Rus");
+	printf_s("Значение переменной в градусах:");
+	scanf_s("%lf", &a);
+	a = (a * 3.14) / 180;
+	printf_s("%lf\n", a);
+	system("pause");
+	return 0;
+}
