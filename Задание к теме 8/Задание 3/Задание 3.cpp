d75c9cb3d37f0a0f34ec5da7acd923e04d9b86f6
re re <kuchernik2002@gmail.com>
2020-11-10 18:44:43 +0300

    Задание 4

---
+++ Задание к теме 8/Задание 3/Задание 3.cpp
@@ -0,0 +1,18 @@
+﻿#include <stdlib.h>
+#include <stdio.h>
+#include <locale.h>
+#include <math.h>
+int main()
+{
+	int A, B, C;
+	setlocale(LC_ALL, "Rus");
+	printf("Длина A:");
+	scanf_s("%d", &A);
+	printf("Длина B:");
+	scanf_s("%d", &B);
+	C = A % B ;
+	printf("%d\n", C);
+	system("pause");
+	return 0;
+}
+
