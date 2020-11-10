187882ffcc09ed35523bacea9181b2f3e12a995d
re re <kuchernik2002@gmail.com>
2020-11-10 18:15:49 +0300

    Задание 2

---
+++ Задание к теме 8/Задание 2/Задание 2.cpp
@@ -0,0 +1,17 @@
+﻿#include <stdlib.h>
+#include <stdio.h>
+#include <locale.h>
+int main()
+{
+	float A, B, C;
+	setlocale(LC_ALL, "Rus");
+	printf_s("Длина A:");
+	scanf_s("%f", &A);
+	printf_s("Длина B:");
+	scanf_s("%f", &B);
+	C = A / B;
+	printf("%lf\n", C);
+	system("pause");
+	return 0;
+}
+
