43f8387cea0926f607992db4aed3dce711ce13bc
re re <kuchernik2002@gmail.com>
2020-11-10 23:26:21 +0300

    Задание 5

---
+++ Задание к теме 9/Задание 5/Задание 5.cpp
@@ -0,0 +1,16 @@
+﻿#include <stdlib.h>
+#include <stdio.h>
+#include <locale.h>
+int main()
+{
+	int A, d;
+	setlocale(LC_ALL, "Rus");
+	printf("Номер года:");
+	scanf_s("%d", &A);
+	d = ((A - 1)/ 100) + 1;
+	printf("%d\n", d);
+	system("pause");
+	return 0;
+}
+
+
