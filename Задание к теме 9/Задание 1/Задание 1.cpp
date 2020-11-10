c650fb8ba1f51bfb7e571335f4551a3310bd93dc
re re <kuchernik2002@gmail.com>
2020-11-10 19:23:39 +0300

    Задание 1

---
+++ Задание к теме 9/Задание 1/Задание 1.cpp
@@ -0,0 +1,15 @@
+﻿#include <stdlib.h>
+#include <stdio.h>
+#include <locale.h>
+int main()
+{
+
+	int A, d;
+	setlocale(LC_ALL, "Rus");
+	printf("Кол-во секунд:");
+	scanf_s("%d", &A);
+	d = A % 60;
+	printf("%d\n", d);
+	system("pause");
+	return 0;
+}
