d93579df0a7c407ba78a2d84ad83f9b7f52d99e2
re re <kuchernik2002@gmail.com>
2020-11-10 23:20:53 +0300

    Задание 4

---
+++ Задание к теме 9/Задание 4/Задание 4.cpp
@@ -0,0 +1,21 @@
+﻿#include <stdlib.h>
+#include <stdio.h>
+#include <locale.h>
+#include<math.h>
+int main()
+{
+	int A, B, C, d, S;
+	setlocale(LC_ALL, "Rus");
+	printf("A:");
+	scanf_s("%d", &A);
+	printf("B:");
+	scanf_s("%d", &B);
+	printf("C:");
+	scanf_s("%d", &C);
+	d = (A / C) * (B / C);
+	printf("%d\n", d);
+	S = A * B - d * sqrt(C);
+	printf("%d\n", S);
+	system("pause");
+	return 0;
+}
