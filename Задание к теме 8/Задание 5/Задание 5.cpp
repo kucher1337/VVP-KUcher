a6b667bc4c7a6a5d5bc2c8b52ffca5ddc2bba29b
re re <kuchernik2002@gmail.com>
2020-11-10 19:11:33 +0300

    Задание 5

--- Задание к теме 8/Задание 5/Задание 5.cpp
+++ Задание к теме 8/Задание 5/Задание 5.cpp
@@ -1,1 +1,15 @@
-﻿
+﻿#include <stdlib.h>
+#include <stdio.h>
+#include <locale.h>
+int main()
+{
+
+	int A, d;
+	setlocale(LC_ALL, "Rus");
+	printf("Трехзначное число:");
+	scanf_s("%d", &A);
+	d = (A % 100) * 10 + A / 100;
+	printf("%d\n", d);
+	system("pause");
+	return 0;
+}
