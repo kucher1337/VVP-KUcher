c29b33dd0c7ffa06264cd54bc6bba4e5c764ccdc
re re <kuchernik2002@gmail.com>
2020-11-10 18:11:45 +0300

    Задание 1

---
+++ Задание к теме 8/Задание 1/Задание 1.cpp
@@ -0,0 +1,15 @@
+﻿#include <stdlib.h>
+#include <stdio.h>
+#include <locale.h>
+int main()
+{
+	float a, b;
+	setlocale(LC_ALL, "Rus");
+	printf_s("Размер в байтах:");
+	scanf_s("%f", &a);
+	b = a / 1024;
+	printf_s("%.lf\n", b);
+	system("pause");
+	return 0;
+}
+
