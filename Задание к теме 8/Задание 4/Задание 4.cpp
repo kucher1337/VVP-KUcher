f9b05d4868fac838bb112b59d4845ef5c7457e36
re re <kuchernik2002@gmail.com>
2020-11-10 19:00:05 +0300

    Задание 4

---
+++ Задание к теме 8/Задание 4/Задание 4.cpp
@@ -0,0 +1,15 @@
+﻿#include <stdlib.h>
+#include <stdio.h>
+#include <locale.h>
+int main()
+{
+
+	int A, d;
+	setlocale(LC_ALL, "Rus");
+	printf("Двузначное число:");
+	scanf_s("%d", &A);
+	d = (A / 10) + (A % 10) * 10;
+	printf("%d\n", d);
+	system("pause");
+	return 0;	
+}
