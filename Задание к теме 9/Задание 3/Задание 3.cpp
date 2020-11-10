165c9087e2c9ba6da385ebd6cd735047e8f72a5e
re re <kuchernik2002@gmail.com>
2020-11-10 23:08:21 +0300

    Задание 3

---
+++ Задание к теме 9/Задание 3/Задание 3.cpp
@@ -0,0 +1,14 @@
+﻿#include <stdlib.h>
+#include <stdio.h>
+#include <locale.h>
+int main()
+{
+	int A, d;
+	setlocale(LC_ALL, "Rus");
+	printf("День в году:");
+	scanf_s("%d", &A);
+	d = (A+3) % 7;
+	printf("%d\n", d);
+	system("pause");
+	return 0;
+}
