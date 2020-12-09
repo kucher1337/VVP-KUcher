c577b4c7608953e504e7d1b41d036572467879c9
re re <kuchernik2002@gmail.com>
2020-12-09 18:34:21 +0300

    Исправил 9.3

--- Задание к теме 9/Задание 3/Задание 3.cpp
+++ Задание к теме 9/Задание 3/Задание 3.cpp
@@ -3,12 +3,13 @@
 #include <locale.h>
 int main()
 {
-	int A, d;
+	int A, d, x;
 	setlocale(LC_ALL, "Rus");
 	printf("День в году:");
-	scanf_s("%d", &A);
-	d = (A+3) % 7;
-	printf("%d\n", d);
-	system("pause");
+	scanf_s("%d", &x);
+	printf("номер дня 1 января:");
+	scanf_s("%d", &d);
+	A = (((x - 1) + (d - 1)) % 7) + 1;
+	printf("день недели: %d\n", A);
 	return 0;
 }
