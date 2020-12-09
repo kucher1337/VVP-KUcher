c75d3e90bb441a6438b88c962e809a38aa982862
re re <kuchernik2002@gmail.com>
2020-12-09 18:48:03 +0300

    Выполнил

---
+++ Задание к теме 10/Задание 2/Задание 2.cpp
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+int main()
+{
+	int x, y, z;
+	setlocale(LC_ALL, "Rus");
+	printf("Значение x:");
+	scanf_s("%d", &x);
+	printf("Значение y:");
+	scanf_s("%d", &y);
+	printf("Значение z:");
+	scanf_s("%d", &z);
+	if ((x < y) && (y < z)) {
+		printf_s("Выражение справедливо");
+	}
+	else {
+		printf_s("Выражение несправедливо");
+	}
+
+	return 0;
+}
---
+++ Задание к теме 10/Задание 3/Задание 3.cpp
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+int main()
+{
+	int x;
+	setlocale(LC_ALL, "Rus");
+	printf("Число :");
+	scanf_s("%d", &x);
+
+	if ((x < 100) && (x > 9) && (x % 2 == 0)) {
+		printf_s("Число двузначное и четное ");
+	}
+	else {
+		printf_s("Число не удовлетворяет условиям ");
+	}
+
+	return 0;
+}
