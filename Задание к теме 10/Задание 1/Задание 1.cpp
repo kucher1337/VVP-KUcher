c4c0872229ab3bb91e70df3f88ea601e740a3b0c
re re <kuchernik2002@gmail.com>
2020-12-09 18:42:53 +0300

    Задание 1

---
+++ Задание к теме 10/Задание 1/Задание 1.cpp
@@ -0,0 +1,20 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+int main()
+{
+	int x, y;
+	setlocale(LC_ALL, "Rus");
+	printf("Значение x:");
+	scanf_s("%d", &x);
+	printf("Значение y:");
+	scanf_s("%d", &y);
+	if ((x > 2) && (y <= 3)) {
+		printf_s("Выражение справедливо");
+	}
+	else {
+		printf_s("Выражение несправедливо");
+	}
+
+	return 0;
+}
