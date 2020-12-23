6d94219a29152aed83fe9353d75cc3d1f15cfe6f
re re <kuchernik2002@gmail.com>
2020-12-23 14:49:23 +0300

    Задание 1

---
+++ Задание к теме 14/Задание 1/Задание 1.cpp
@@ -0,0 +1,28 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+int main()
+{
+	double x, y, z, k, B, A;
+	setlocale(LC_ALL, "Rus");
+	printf("A:");
+	scanf_s("%lf", &A);
+	printf("B:");
+	scanf_s("%lf", &B);
+
+	if (A < B) {
+		for (z = 1; z <= A; z++) {
+			k = A;
+			printf_s("Реузльтат: %lf", k);
+		}
+		for (x = B; x >= 1; x--) {
+		y= B;
+			printf_s("Результат: %lf", y);
+		}
+
+	}
+	else
+		printf_s("Неверно");
+
+	return 0;
+}
